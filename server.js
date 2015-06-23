5821d262e28ce7b11a77c2eed64df8461e6934cf
dbaylyuvic <dbayly@uvic.ca>
2015-06-23 13:49:23 -0700

    This is a basic server.js, It should show Index.html

---
+++ server.js
@@ -0,0 +1,11 @@
+//Packages
+var express    = require('express');
+var app = express();
+var path = require('path');
+app.get('\' function (req,res){
+	
+	res.sendFile(path.join(__dirname + '/app/views/index.html'));
+
+}
+ app.listen(1337);
+console.log('1337 is the magic port!');
