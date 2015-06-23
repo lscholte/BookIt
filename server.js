4e9894adf680f8cd4f691af82aa74f36bea83bf9
dbaylyuvic <dbayly@uvic.ca>
2015-06-23 14:05:07 -0700

    Fix a few minor mistakes in the first one

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 //Packages
-var express    = require('express');
+var express = require('express');
 var app = express();
 var path = require('path');
-app.get('\' function (req,res){
-	
-	res.sendFile(path.join(__dirname + '/app/views/index.html'));
-
-}
+app.get('/', function(req,res){
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
  app.listen(1337);
 console.log('1337 is the magic port!');
