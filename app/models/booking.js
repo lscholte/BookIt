9ad7c17193ca140bab662db4d0f4c55cd47e5ad0
Simon Griffith <simgriffi@gmail.com>
2015-06-23 13:45:00 -0700

    Updated getters and setters for booking schema

--- app/models/booking.js
+++ app/models/booking.js
@@ -1,9 +1,55 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
+var EquipmentSchema = require('equipment');
+var RoomSchema = require('room');
+var UserSchema = require('user');
 
 var BookingSchema = new Schema({
+	user: {type : mongoose.Schema.ObjectId, ref : 'User' , required: true},
+	room: {type : mongoose.Schema.ObjectId, ref : 'Room' , required: true},
+	equipment: [{type : mongoose.Schema.ObjectId, ref : 'Equipment' }],
 	startDate: {type: Date, required: true},
 	endDate:  {type: Date, required: true}
 });
+
+BookingSchema.methods.setUser = function(user){
+	this.user = user._id;
+};
+
+BookingSchema.methods.getUser = function(){
+	return this.populate('user');
+};
+
+BookingSchema.methods.setRoom = function(room){
+	this.room = room._id;
+};
+
+BookingSchema.methods.getRoom = function(){
+	return this.populate('room');	
+};
+
+BookingSchema.methods.setEquipment = function(equipment){
+	this.equipment = equipment;
+};
+
+BookingSchema.methods.getEquipment = function(){
+	return this.populate('equipment');
+};
+
+BookingSchema.methods.setStartDate = function(date){
+	this.startDate = date;
+};
+
+BookingSchema.methods.getStartDate = function(){
+	return this.startDate;
+};
+
+BookingSchema.methods.setEndDate = function(date){
+	this.endDate = date;
+};
+
+BookingSchema.methods.getEndDate = function(){
+	return this.endDate;
+};
 
 module.exports = mongoose.model('Booking', BookingSchema);
