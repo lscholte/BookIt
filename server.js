5412936c9741531751cfa00f4436644ea2aeb78b
dbaylyuvic <dbayly@uvic.ca>
2015-06-23 14:34:14 -0700

    Added routes, fixed path for index.html

--- server.js
+++ server.js
@@ -2,8 +2,10 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var apiRoutes = require('./app/routes/api')(app, express);
+app.use('/api', apiRoutes);
 app.get('/', function(req,res){
-	res.sendFile(path.join(__dirname + '/index.html'));
+	res.sendFile(path.join(__dirname + '/app/views/index.html'));
 });
  app.listen(1337);
 console.log('1337 is the magic port!');
