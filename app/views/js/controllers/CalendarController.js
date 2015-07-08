f39dfc0c29ea8925292e6bc9040e6c4f35c54b1c
Simon Griffith <simgriffi@gmail.com>
2015-07-08 15:19:24 -0700

    Added calendarService, main page displays # of bookings that day.

--- app/views/js/controllers/CalendarController.js
+++ app/views/js/controllers/CalendarController.js
@@ -1,18 +1,37 @@
-angular.module('calendarCtrl', [])
+angular.module('calendarCtrl', ['calendarService'])
 
-.controller('CalendarController', function($scope, $location, Auth){
+.controller('CalendarController', function($scope, Calendar){
 	
-	this.date = new Date();
+	var vm = this;
+	vm.date = new Date();
+	
+	Calendar.all().success(function(data){
+		vm.bookings = data;
+	});
+	
+	this.getBookingCount = function(day){
+		day = new Date(day);
+		var count = 0;
+		for(var index in vm.bookings){
+			var bookingDate = new Date(vm.bookings[index].startDate).toDateString();
+			var desiredDate = day.toDateString();
+			if( bookingDate == desiredDate){
+				count++;
+			}
+		}
+		return count;
+	};
 	
 	this.weekStart = function(){
-  		var day = this.date.getDay();
-     	var diff = this.date.getDate() - day;
-  		return new Date(this.date.setDate(diff));
+  		var day = vm.date.getDay();
+     	var diff = vm.date.getDate() - day;
+  		return new Date(vm.date.setDate(diff));
 	};
 	
 	this.weekEnd = function(){
-  		var day = this.date.getDay();
-     	var diff = this.date.getDate() - day;
-  		return new Date(this.date.setDate(diff + 6));
+  		var day = vm.date.getDay();
+     	var diff = vm.date.getDate() - day;
+  		return new Date(vm.date.setDate(diff + 6));
 	};
+	
 });
