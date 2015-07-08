a0c4945561a746294a5925c018b4af74fd16d8f3
Simon Griffith <simgriffi@gmail.com>
2015-07-08 09:20:36 -0700

    Main view "calendar" header now shows date range of the week

--- app/views/js/controllers/CalendarController.js
+++ app/views/js/controllers/CalendarController.js
@@ -3,5 +3,16 @@ angular.module('calendarCtrl', [])
 .controller('CalendarController', function($scope, $location, Auth){
 	
 	this.date = new Date();
-	this.hours = ["8:00", "9:00", "10:00", "11:00", "12:00", "1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00"];
+	
+	this.weekStart = function(){
+  		var day = this.date.getDay();
+     	var diff = this.date.getDate() - day;
+  		return new Date(this.date.setDate(diff));
+	};
+	
+	this.weekEnd = function(){
+  		var day = this.date.getDay();
+     	var diff = this.date.getDate() - day;
+  		return new Date(this.date.setDate(diff + 6));
+	};
 });
