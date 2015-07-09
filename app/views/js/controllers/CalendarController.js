cb541fd3236d87d8727bbd253db43081a0f05bf0
Simon Griffith <simgriffi@gmail.com>
2015-07-08 18:50:26 -0700

    Calendar now shows remaining bookings

--- app/views/js/controllers/CalendarController.js
+++ app/views/js/controllers/CalendarController.js
@@ -11,12 +11,22 @@ angular.module('calendarCtrl', ['calendarService'])
 	
 	this.getBookingCount = function(day){
 		day = new Date(day);
-		var count = 0;
+		var count;
+		
+		//Figure out how many booking slots are available hypothetically, then subtract bookings that are already made
+		//ToDo: ideally the initial value for count would use an api call for calculation
+		if(day.getDay() == 0 || day.getDay() == 6){
+			count = 70; // On weekends rooms open 11-6 (7 hours), and there's 10 rooms, so 70 = 7*10
+		}
+		else{
+			count = 140; //weekdays, rooms open 8-10 (14 hours), and there's 10 rooms, so 140 = 14*10
+		}
+		
 		for(var index in vm.bookings){
 			var bookingDate = new Date(vm.bookings[index].startDate).toDateString();
 			var desiredDate = day.toDateString();
 			if( bookingDate == desiredDate){
-				count++;
+				count--;
 			}
 		}
 		return count;
