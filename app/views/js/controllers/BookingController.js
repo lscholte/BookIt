97977b39e3201e693a504b8bedeaa4a07d4b2f32
lscholte <lscholte@uvic.ca>
2015-07-10 21:12:11 -0700

    Cleans up Booking controller and makes names more intuitive

--- app/views/js/controllers/BookingController.js
+++ app/views/js/controllers/BookingController.js
@@ -2,88 +2,77 @@ angular.module('bookingCtrl', ["activeBookingService"])
 
 .controller('BookingController', function($scope, $location, Auth, ActiveBooking){
             
-            //TODO: This needs some serious cleaning up.
             //All of this stuff currently relates to
             //the quickbook feature, so maybe it is worth
             //making this the quickbook controller and
             //creating a new controller for the other
             //booking process
             
-            this.days = [];
-            this.startTimes = [];
-            this.endTimes = []
+            this.dayDropdownItems = [];
+            this.startTimeDropdownItems = [];
+            this.endTimeDropdownItems = []
             
             this.selectedDayText = "";
             this.selectedStartTimeText = "";
             this.selectedEndTimeText = "";
             
-            this.startTime = null;
-            this.endTime = null;
+            this.bookingStartTime = null;
+            this.bookingEndTime = null;
             
             //this.activeBooking = ActiveBooking.getActiveBooking();
             this.activeBooking = function(){
                 return ActiveBooking.activeBooking;
             };
             
-            this.setParameters = function() {
-                this.setPotentialDays();
-                this.setStartTimes();
-                this.setEndTimes();
-            };
-            
-            this.setPotentialDays = function() {
+            this.setDayDropdownItems = function() {
                 var d = new Date();
             
                 for(var i = 0; i < 14; ++i) {
-                    this.days.push(d.toDateString())
+                    this.dayDropdownItems.push(d.toDateString())
                     d.setMilliseconds(d.getMilliseconds() + 86400000);
                 }
-                this.selectedDayText = this.days[0];
-                this.setStartEndDay();
+                this.selectedDayText = this.dayDropdownItems[0];
             };
             
-            this.setStartEndDay = function() {
-                this.startTime = new Date(this.selectedDayText);
-                this.endTime = new Date(this.selectedDayText);
+            this.setBookingStartTime = function() {
+                this.bookingStartTime = new Date(this.selectedDayText + " " + this.selectedStartTimeText);
             }
             
-            this.setStartTime = function() {
-                this.startTime = new Date(this.startTime.toDateString() + " " + this.selectedStartTimeText);
-            }
-            
-            this.setEndTime = function() {
-                this.endTime = new Date(this.endTime.toDateString() + " " + this.selectedEndTimeText);
+            this.setBookingEndTime = function() {
+                this.bookingEndTime = new Date(this.selectedDayText + " " + this.selectedEndTimeText);
             }
 
             
-            this.setStartTimes = function() {
+            this.setStartTimeDropdownItems = function() {
                 var day = new Date(this.selectedDayText);
                 if(day.getDay() == 0 || day.getDay() == 6) {
-                    this.startTimes = ["11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM"];
+                    this.startTimeDropdownItems = ["11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM"];
                 }
                 else {
-                    this.startTimes = ["8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM", "7:00 PM", "8:00 PM", "9:00 PM"];
+                    this.startTimeDropdownItems = ["8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM", "7:00 PM", "8:00 PM", "9:00 PM"];
                 }
-                this.selectedStartTimeText = this.startTimes[0];
-                this.setStartTime();
+                this.selectedStartTimeText = this.startTimeDropdownItems[0];
+                this.setBookingStartTime();
             
             };
             
-            this.setEndTimes = function() {
-                var hour = this.startTime.getHours() + 1;
+            this.setEndTimeDropdownItems = function() {
+            
+                var hour = this.bookingStartTime.getHours() + 1;
                 if(hour > 12) {
                     hour -= 12;
-                    this.endTimes = [hour + ":00 PM"];
+                    this.endTimeDropdownItems = [hour + ":00 PM"];
                 }
                 else {
                     if(hour == 12) {
-                        this.endTimes = [hour + ":00 PM"];
+                        this.endTimeDropdownItems = [hour + ":00 PM"];
                     }
                     else {
-                        this.endTimes = [hour + ":00 AM"];
+                        this.endTimeDropdownItems = [hour + ":00 AM"];
                     }
                 }
-                this.selectedEndTimeText = this.endTimes[0];
+                this.selectedEndTimeText = this.endTimeDropdownItems[0];
+                this.setBookingEndTime();
             };
             
             });
