2f7e7a8f3cb22bba63b882d0ca0a3b4f6094df56
lscholte <lscholte@uvic.ca>
2015-07-13 01:02:42 -0700

    Fixes indentation

--- app/views/js/services/quickBookService.js
+++ app/views/js/services/quickBookService.js
@@ -1,49 +1,49 @@
 angular.module('quickBookService', [])
 
-	.factory('quickBook', function($http){
-
-		var quickBookFactory = {};
-
-		//startDate and endDate expected as javascript Date Objects. equipment is somthing like [laptop, projecter] to book both a laptop and a projector. returns booking id if succesful
-		quickBookFactory.book = function(startDate, endDate, username, equipment, callback){
-
-			//check for equipment
-			var equipmentIds = [];
-			$http.get('/api/equipment?startDate=' + startDate.getTime() + '&endDate=' + endDate.getTime()).success(function(data, status, headers, config){
-				for(var i = 0; i < equipment.length; i++){
-					for(var j = 0; j < data.length; j++){
-						if(equipment[i] == data[j].equipmentType){
-							equipmentIds.push(data[j]._id);
-							break;
-						}
-					}
-				}
-				if(equipmentIds.length != equipment.length){
-					callback(null, "A requested piece of equipment is not available during this time.");
-				}
-				$http.get('/api/rooms?startDate=' + startDate.getTime() + '&endDate=' + endDate.getTime()).success(function(data, status, headers, config){
-					//there's no rooms in this timeslot
-					if(data.length == 0){
-						callback(null, "There are no available rooms during the requested time period");
-					}
-					//else make the booking
-					$http.post('/api/bookings', {startDate: startDate.getTime(), endDate: endDate.getTime(), username: username, roomNumber: data[0].roomNumber}).success(function(data, status, headers, config){
-						var bookingID = data.id;
-						if(equipmentIds.length > 0){
-							$http.put('/api/bookings/equipment/' + bookingID, {equipmentID:equipmentIds}).success(function(data, status, headers, config){
-								callback(bookingID, null);
-							});
-						}
-						else{
-							 callback(bookingID, null);
-						}
-					}).error(function(data, status, headers, config){
-	                    callback(null, status + ": " + data);
-					});
-				});
-			});
-		};
-
-		return quickBookFactory;
-
-	});
+        .factory('quickBook', function($http){
+            
+            var quickBookFactory = {};
+            
+            //startDate and endDate expected as javascript Date Objects. equipment is somthing like [laptop, projecter] to book both a laptop and a projector. returns booking id if succesful
+            quickBookFactory.book = function(startDate, endDate, username, equipment, callback){
+                
+                //check for equipment
+                var equipmentIds = [];
+                $http.get('/api/equipment?startDate=' + startDate.getTime() + '&endDate=' + endDate.getTime()).success(function(data, status, headers, config){
+                    for(var i = 0; i < equipment.length; i++){
+                        for(var j = 0; j < data.length; j++){
+                            if(equipment[i] == data[j].equipmentType){
+                                equipmentIds.push(data[j]._id);
+                                break;
+                            }
+                        }
+                    }
+                    if(equipmentIds.length != equipment.length){
+                        callback(null, "A requested piece of equipment is not available during this time.");
+                    }
+                    $http.get('/api/rooms?startDate=' + startDate.getTime() + '&endDate=' + endDate.getTime()).success(function(data, status, headers, config){
+                        //there's no rooms in this timeslot
+                        if(data.length == 0){
+                            callback(null, "There are no available rooms during the requested time period");
+                        }
+                        //else make the booking
+                        $http.post('/api/bookings', {startDate: startDate.getTime(), endDate: endDate.getTime(), username: username, roomNumber: data[0].roomNumber}).success(function(data, status, headers, config){
+                            var bookingID = data.id;
+                            if(equipmentIds.length > 0){
+                                $http.put('/api/bookings/equipment/' + bookingID, {equipmentID:equipmentIds}).success(function(data, status, headers, config){
+                                    callback(bookingID, null);
+                                });
+                            }
+                            else{
+                                callback(bookingID, null);
+                            }
+                        }).error(function(data, status, headers, config){
+                            callback(null, status + ": " + data);
+                        });
+                    });
+                });
+            };
+            
+            return quickBookFactory;
+            
+        });
